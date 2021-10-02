db0705df2af4bc4bc37b14fc0a28b7ae6f2ec879
jiekurumi02 <xdieu2k2@gmail.com>
2021-10-02 09:52:51 +0700

    sua code lan 3

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main ()
 {
     cout<<"Hello World !!!"<<endl;
-    cout<<"Sua code xem the nao ?";
+    cout<<"Sua code xem the nao ?"<<endl;
+    cout<<"Kho chiu vl !";
     return 0;
 }
